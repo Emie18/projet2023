9913bc29a71a41962fc65d62bce549f8a34fbd19
Emie18 <lalala.emiliat@gmail.com>
2023-03-20 11:57:06 +0100

    fehzehj

--- projet/consigne.c
+++ projet/consigne.c
@@ -2,10 +2,27 @@
 
    float consigne(float thermostatPrec_f)
    {
-   
+      FILE* fichierverrou = fopen(".verrouconsigne", "w");
+ 
+
+      FILE* fichier = NULL;
+      float consigne = 0; 
+      fichier = fopen("consigne.txt", "r");
+
+      if (fichier != NULL)
+      {
+         fscanf(fichier, "%f", &consigne);
+         printf("consigne: %f",consigne);
+
+         fclose(fichier);
+      }else{
+          printf("non:");
+      }
+    //if(access(".verroudata",0)!=0){
       float thermostat_f=0;
-   
-     
-      return thermostat_f;
-   
+      //return thermostat_f;
+      remove(".verrouconsigne");
+      return consigne;
+    //}
+
    }
